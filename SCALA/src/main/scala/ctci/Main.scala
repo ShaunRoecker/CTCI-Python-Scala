f7b89aba277a4c5ea4d6c583bb65fe01d993ea71
s-roecker <shaun.roecker@gmail.com>
2023-05-07 15:02:43 -0400

    Problem 1 - isUnique (char in String)

--- SCALA/src/main/scala/ctci/Main.scala
+++ SCALA/src/main/scala/ctci/Main.scala
@@ -1,5 +1,21 @@
 package ctci
 
+import ctci.chapter1.IsUnique._
+
+
 @main
 def main = 
-    println("CTCI")
+    println("CTCI Scala")
+
+    println { Console.CYAN + "*" * 50 + "\n" + Console.RESET }
+    println { Console.GREEN + "CHAPTER 1" + Console.RESET }
+
+    println(isUnique3("hasdups")) // false
+    println(isUnique3("nodups")) // true
+
+    println(isUnique4("hasdups")) // false
+    println(isUnique4("nodups")) // true
+
+
+
+
